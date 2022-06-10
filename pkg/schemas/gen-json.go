7b3e1518ebcb2009002e1630ee5908a072ac3f61
Adrian Riobo Lorenzo <ariobolo@redhat.com>
2022-06-10 11:09:29 +0200

    match interface for generators

--- pkg/schemas/gen-json.go
+++ pkg/schemas/gen-json.go
@@ -10,11 +10,11 @@ import (
 	"github.com/adrianriobo/dater/pkg/schemas"
 )
 
-// os.Args[1] input filename
-// os.Args[2] outout filename
+// os.Args[1] sourcePath
+// os.Args[2] targetPath
 // os.Args[3] package name
 func main() {
-	if err := schemas.GenerateFromJSONSchema(os.Args[1], os.Args[2], os.Args[3]); err != nil {
+	if err := schemas.StructFromJSONSchema(os.Args[1], os.Args[2], os.Args[3]); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
--- pkg/schemas/schemas.go
+++ pkg/schemas/schemas.go
@@ -2,46 +2,95 @@ package schemas
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/a-h/generate"
+	"github.com/adrianriobo/dater/pkg/util/logging"
 	"github.com/xuri/xgen"
-	"golang.org/x/exp/slices"
 )
 
 // //go:generate go run gen-json.go xunit/xunit.json xunit/xunit.go xunit
-//go:generate go run gen-xsd.go xunit xunit.xsd xunit xunit
+//go:generate go run gen-xsd.go xunit xunit xunit
 
-func GenerateFromXSD(inputFolder, xsdFileName, outputFolder, packageName string) error {
-	files, err := xgen.GetFileList(inputFolder)
+func StructFromXSD(sourceFolder, outputFolder, packageName string) error {
+	files, err := ioutil.ReadDir(sourceFolder)
 	if err != nil {
 		return err
 	}
-	file := getXSDFile(files, fmt.Sprintf("%s/%s", inputFolder, xsdFileName))
-	if file == "" {
-		return fmt.Errorf("file %s not found", xsdFileName)
-	}
-
-	if err = xgen.NewParser(&xgen.Options{
-		FilePath:            file,
-		InputDir:            inputFolder,
-		OutputDir:           outputFolder,
-		Lang:                "Go",
-		Package:             packageName,
-		IncludeMap:          make(map[string]bool),
-		LocalNameNSMap:      make(map[string]string),
-		NSSchemaLocationMap: make(map[string]string),
-		ParseFileList:       make(map[string]bool),
-		ParseFileMap:        make(map[string][]interface{}),
-		ProtoTree:           make([]interface{}, 0),
-		RemoteSchema:        make(map[string][]byte),
-	}).Parse(); err != nil {
-		return err
+	for _, file := range files {
+		fileName := file.Name()
+		if file.IsDir() {
+			if err := StructFromXSD(
+				fmt.Sprintf("%s/%s", sourceFolder, fileName),
+				outputFolder,
+				packageName); err != nil {
+				return err
+			}
+		} else {
+			if filepath.Ext(fileName) != ".xsd" {
+				logging.Errorf("%s extension is not supported", filepath.Ext(fileName))
+				break
+			}
+			if err = xgen.NewParser(&xgen.Options{
+				FilePath:            fmt.Sprintf("%s/%s", sourceFolder, fileName),
+				InputDir:            sourceFolder,
+				OutputDir:           outputFolder,
+				Lang:                "Go",
+				Package:             packageName,
+				IncludeMap:          make(map[string]bool),
+				LocalNameNSMap:      make(map[string]string),
+				NSSchemaLocationMap: make(map[string]string),
+				ParseFileList:       make(map[string]bool),
+				ParseFileMap:        make(map[string][]interface{}),
+				ProtoTree:           make([]interface{}, 0),
+				RemoteSchema:        make(map[string][]byte),
+			}).Parse(); err != nil {
+				return err
+			}
+		}
 	}
 	return nil
 }
 
-func GenerateFromJSONSchema(inputFileName, outputFileName, packageName string) error {
+func StructFromJSONSchema(sourceFolder, outputFolder, packageName string) error {
+	files, err := ioutil.ReadDir(sourceFolder)
+	if err != nil {
+		return err
+	}
+	for _, file := range files {
+		fileName := file.Name()
+		if file.IsDir() {
+			if err := StructFromJSONSchema(
+				fmt.Sprintf("%s/%s", sourceFolder, fileName),
+				outputFolder,
+				packageName); err != nil {
+				return err
+			}
+		} else {
+			switch ext := filepath.Ext(fileName); ext {
+			case ".yaml":
+				// https://github.com/kubernetes-sigs/yaml
+				fmt.Println("OS X.")
+			case ".json":
+				targetFileName := fmt.Sprintf("%s.go", strings.TrimSuffix(fileName, ext))
+				if err := generateFromJSONSchema(
+					file.Name(),
+					targetFileName,
+					packageName); err != nil {
+					return nil
+				}
+			default:
+				logging.Errorf("%s extension is not supported", ext)
+			}
+		}
+	}
+	return nil
+}
+
+func generateFromJSONSchema(inputFileName, outputFileName, packageName string) error {
 	schemas, err := generate.ReadInputFiles(append([]string{}, inputFileName), true)
 	if err != nil {
 		return err
@@ -57,13 +106,4 @@ func GenerateFromJSONSchema(inputFileName, outputFileName, packageName string) e
 	}
 	generate.Output(writer, g, packageName)
 	return nil
-}
-
-func getXSDFile(files []string, xsdFileName string) string {
-	idx := slices.IndexFunc(files,
-		func(e string) bool { return e == xsdFileName })
-	if idx == -1 {
-		return ""
-	}
-	return files[idx]
 }
